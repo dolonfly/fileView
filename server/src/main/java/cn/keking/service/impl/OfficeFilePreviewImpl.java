6990d919a283fec939c2f9b3e22a35da1161e874
dll <kaixin890312@163.com>
2023-12-14 20:39:25 +0800

    fix

--- server/src/main/java/cn/keking/service/impl/OfficeFilePreviewImpl.java
+++ server/src/main/java/cn/keking/service/impl/OfficeFilePreviewImpl.java
@@ -71,7 +71,8 @@ public class OfficeFilePreviewImpl implements FilePreview {
                 }
             }
         }
-        if (forceUpdatedCache || !fileHandlerService.listConvertedFiles().containsKey(pdfName) || !DownloadUtils.fileExist(fileAttribute, fileName) || !ConfigConstants.isCacheEnabled()) {
+        if (DownloadUtils.fileExist(fileAttribute, fileName) && (
+                forceUpdatedCache || !fileHandlerService.listConvertedFiles().containsKey(pdfName) || !ConfigConstants.isCacheEnabled())) {
         // 下载远程文件到本地，如果文件在本地已存在不会重复下载
         ReturnResponse<String> response = DownloadUtils.downLoad(fileAttribute, fileName);
         if (response.isFailure()) {
